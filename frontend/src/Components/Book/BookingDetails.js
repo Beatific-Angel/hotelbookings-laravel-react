40a402825abbab18d1606af52633406d8863e3ca
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-04-16 05:46:24 +0300

    update
    update

--- frontend/src/Components/Book/BookingDetails.js
+++ frontend/src/Components/Book/BookingDetails.js
@@ -59,52 +59,6 @@ function BookingDetails({ booking, setBooking }) {
                                 <span>{price}</span>$
                             </span>
                         </div>
-                        <div className="mt-8">
-                            <div className="flex flex-col md:flex-row md:justify-between items-center">
-                                <label
-                                    htmlFor="check-in"
-                                    className="mr-5 text-gray-900 md:w-1/4"
-                                >
-                                    Check-in
-                                </label>
-                                <input
-                                    id="check-in"
-                                    className="mt-2 md:mt-0 px-6 py-3 wf w-full md:w-3/4"
-                                    type="date"
-                                    value={booking.check_in}
-                                    onChange={(e) => {
-                                        calcPrice();
-                                        setBooking({
-                                            ...booking,
-                                            check_in: e.target.value,
-                                            amount: price
-                                        });
-                                    }}
-                                />
-                            </div>
-                            <div className="flex flex-col md:flex-row md:justify-between items-center mt-4">
-                                <label
-                                    htmlFor="check-out"
-                                    className="mr-5 text-gray-900 md:w-1/4"
-                                >
-                                    Check-out
-                                </label>
-                                <input
-                                    id="check-out"
-                                    className="mt-2 md:mt-0 px-6 py-3 wf w-full md:w-3/4"
-                                    type="date"
-                                    value={booking.check_out}
-                                    onChange={(e) => {
-                                        calcPrice();
-                                        setBooking({
-                                            ...booking,
-                                            check_out: e.target.value,
-                                            amount: price
-                                        });
-                                    }}
-                                />
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
