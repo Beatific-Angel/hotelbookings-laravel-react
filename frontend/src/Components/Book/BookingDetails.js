99295b041319d50735384e5b32d4783313b4b79a
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-04-20 05:51:55 +0300

    update
    update

--- frontend/src/Components/Book/BookingDetails.js
+++ frontend/src/Components/Book/BookingDetails.js
@@ -2,6 +2,30 @@ import React, { useState, useEffect } from "react";
 import useSecureLs from "../Global/useSecureLs";
 
 function BookingDetails({ booking, setBooking }) {
+    const [price, setPrice] = useState(0);
+    const [room_id] = useSecureLs("room_id");
+    const [room_name] = useSecureLs("room_name");
+    const [room_price] = useSecureLs("room_price");
+    const [room_image] = useSecureLs("room_image");
+    const room = {
+        room_id,
+        name: room_name,
+        price: room_price,
+        image: room_image
+    };
+
+    const calcPrice = () => {
+        if (booking.check_in !== "" && booking.check_out !== "") {
+            const oneDay = 24 * 60 * 60 * 1000;
+            const check_out = new Date(booking.check_out);
+            const check_in = new Date(booking.check_in);
+            const diffDays = Math.floor((check_out - check_in) / oneDay);
+
+            if (diffDays > 0) {
+                setPrice(room.price * diffDays);
+            }
+        }
+    };
 
     useEffect(() => {
         calcPrice();
@@ -35,7 +59,52 @@ function BookingDetails({ booking, setBooking }) {
                                 <span>{price}</span>$
                             </span>
                         </div>
-
+                        <div className="mt-8">
+                            <div className="flex flex-col md:flex-row md:justify-between items-center">
+                                <label
+                                    htmlFor="check-in"
+                                    className="mr-5 text-gray-900 md:w-1/4"
+                                >
+                                    Check-in
+                                </label>
+                                <input
+                                    id="check-in"
+                                    className="mt-2 md:mt-0 px-6 py-3 wf w-full md:w-3/4"
+                                    type="date"
+                                    value={booking.check_in}
+                                    onChange={(e) => {
+                                        calcPrice();
+                                        setBooking({
+                                            ...booking,
+                                            check_in: e.target.value,
+                                            amount: price
+                                        });
+                                    }}
+                                />
+                            </div>
+                            <div className="flex flex-col md:flex-row md:justify-between items-center mt-4">
+                                <label
+                                    htmlFor="check-out"
+                                    className="mr-5 text-gray-900 md:w-1/4"
+                                >
+                                    Check-out
+                                </label>
+                                <input
+                                    id="check-out"
+                                    className="mt-2 md:mt-0 px-6 py-3 wf w-full md:w-3/4"
+                                    type="date"
+                                    value={booking.check_out}
+                                    onChange={(e) => {
+                                        calcPrice();
+                                        setBooking({
+                                            ...booking,
+                                            check_out: e.target.value,
+                                            amount: price
+                                        });
+                                    }}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
