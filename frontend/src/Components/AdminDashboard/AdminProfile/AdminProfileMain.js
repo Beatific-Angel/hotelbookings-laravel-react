11a17c36f342853613aaf08174d2c745c2355db2
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-05-07 05:06:54 +0300

    update
    update

--- frontend/src/Components/AdminDashboard/AdminProfile/AdminProfileMain.js
+++ frontend/src/Components/AdminDashboard/AdminProfile/AdminProfileMain.js
@@ -15,7 +15,19 @@ function AdminProfileMain() {
     const state = useSelector((state) => state);
     let { id } = useParams();
 
+    useEffect(() => {
+        getUser(dispatch, state.auth.token, id);
+        document.title = `Profile`;
+    }, []); // eslint-disable-line
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setSuccess(dispatch, null);
+        }, 3000);
+        return () => clearTimeout(timer);
+    }, [state.users.success]); // eslint-disable-line
+
+    return (
         <div className="w-full md:w-6/12 xl:w-8/12 md:ml-5 bg-gray-200 p-5 rounded-sm  rounded-t-none md:rounded-t-md">
             {state.users.loading && <Loading />}
 
