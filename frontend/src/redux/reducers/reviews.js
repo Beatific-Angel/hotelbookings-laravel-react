3600c361f36bef8348bf199887abcf4316f8fad1
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-06-15 05:34:34 +0300

    update
    update

--- frontend/src/redux/reducers/reviews.js
+++ frontend/src/redux/reducers/reviews.js
@@ -1,3 +1,15 @@
+import {
+    GET_USER_REVIEWS,
+    ADD_REVIEW,
+    UPDATE_REVIEW,
+    DELETE_REVIEW,
+    SET_LOADING,
+    GET_HOTEL_REVIEWS,
+    GET_ALL_REVIEWS,
+    SET_SUCCESS,
+    HAS_BOOKED
+} from "../actionTypes";
+
 const initialState = {
     reviews: [],
     success: null,
@@ -56,7 +68,35 @@ export default function (state = initialState, action) {
                 }
             };
         }
-    
+        case GET_HOTEL_REVIEWS: {
+            return {
+                ...state,
+                reviews: action.payload.success
+                    ? action.payload.reviews.data
+                    : [...state.reviews],
+                pagination: {
+                    current_page: action.payload.reviews.current_page,
+                    first_page_url: action.payload.reviews.first_page_url,
+                    from: action.payload.reviews.from,
+                    last_page: action.payload.reviews.last_page,
+                    last_page_url: action.payload.reviews.last_page_url,
+                    next_page_url: action.payload.reviews.next_page_url,
+                    path: action.payload.reviews.path,
+                    per_page: action.payload.reviews.per_page,
+                    prev_page_url: action.payload.reviews.prev_page_url
+                }
+            };
+        }
+        case ADD_REVIEW: {
+            return {
+                ...state,
+                reviews: action.payload.success
+                    ? [...state.reviews, action.payload.review]
+                    : [...state.reviews],
+                success: action.payload.success ? true : false,
+                errors: action.payload.success ? null : action.payload.errors
+            };
+        }
         case UPDATE_REVIEW: {
             return {
                 ...state,
@@ -77,5 +117,20 @@ export default function (state = initialState, action) {
                 hasBooked: action.payload > 0 ? true : false
             };
         }
+        case SET_LOADING: {
+            return {
+                ...state,
+                loading: action.payload
+            };
+        }
+
+        case SET_SUCCESS: {
+            return {
+                ...state,
+                success: action.payload
+            };
+        }
+        default:
+            return state;
     }
 }
