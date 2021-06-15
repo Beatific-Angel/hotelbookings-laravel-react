3fe13bedc2ac1293da2739e966ef7f4bbfef6d4d
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-06-15 04:29:09 +0300

    update
    update

--- frontend/src/Components/Book/PaymentForm.js
+++ frontend/src/Components/Book/PaymentForm.js
@@ -81,18 +81,6 @@ function PaymentForm({ booking, setBooking, maxLengthCheck, onSubmitHandler }) {
                     </div>
                 </div>
 
-                <span aria-hidden="true" className="text-sm text-gray-500 mt-2 inline-block">
-                    * required fields
-                </span>
-
-                <h2 className="pl-5 text-2xl">Step 3: Place Order</h2>
-
-                <button
-                    type="submit"
-                    className="bg-orange-700 mt-5 py-2 px-6 text-3xl text-gray-200 block w-10/12 mx-auto hover:bg-orange-900"
-                >
-                    BOOK
-                </button>
             </form>
         </>
     );
