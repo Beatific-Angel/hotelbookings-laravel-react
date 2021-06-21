39909e3e1ec970d9089e6d61db2e182f49dfd2ce
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-06-21 05:35:56 +0300

    update
    update

--- frontend/src/redux/reducers/search.js
+++ frontend/src/redux/reducers/search.js
@@ -1,3 +1,4 @@
+import { SEARCH, GET_SEARCH_DATA, SET_LOADING } from "../actionTypes";
 
 const initialState = {
     hotels: [],
@@ -22,6 +23,23 @@ const initialState = {
 export default function (state = initialState, action) {
     switch (action.type) {
         case SEARCH: {
+            return {
+                ...state,
+                hotels: action.payload.hotels.data,
+                success: action.payload.success ? true : false,
+                errors: action.payload.success ? null : action.payload.errors,
+                pagination: {
+                    current_page: action.payload.hotels.current_page,
+                    first_page_url: action.payload.hotels.first_page_url,
+                    from: action.payload.hotels.from,
+                    last_page: action.payload.hotels.last_page,
+                    last_page_url: action.payload.hotels.last_page_url,
+                    next_page_url: action.payload.hotels.next_page_url,
+                    path: action.payload.hotels.path,
+                    per_page: action.payload.hotels.per_page,
+                    prev_page_url: action.payload.hotels.prev_page_url
+                }
+            };
         }
         case GET_SEARCH_DATA: {
             return {
