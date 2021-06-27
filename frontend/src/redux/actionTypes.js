0d6061c7b5df3dbe8c940f0a45d2f0ebcf29e314
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-06-27 05:37:23 +0300

    update
    update

--- frontend/src/redux/actionTypes.js
+++ frontend/src/redux/actionTypes.js
@@ -15,29 +15,3 @@ export const ADD_ROOM = "ADD_ROOM";
 export const UPDATE_ROOM = "UPDATE_ROOM";
 export const DELETE_ROOM = "DELETE_ROOM";
 export const GET_ALL_ROOMS = "GET_ALL_ROOMS";
-
-export const GET_ALL_BOOKINGS = "GET_ALL_BOOKINGS";
-export const GET_ALL_REVIEWS = "GET_ALL_REVIEWS";
-
-export const GET_USER = "GET_USER";
-export const UPDATE_USER_INFO = "UPDATE_USER_INFO";
-export const UPDATE_USER_EMAIL = "UPDATE_USER_EMAIL";
-export const UPDATE_USER_PASSWORD = "UPDATE_USER_INFO";
-
-export const GET_USER_BOOKINGS = "GET_USER_BOOKINGS";
-export const GET_USER_REVIEWS = "GET_USER_REVIEWS";
-
-export const ADD_BOOKING = "ADD_BOOKING";
-export const SET_LOADING = "SET_LOADING";
-export const CONFRIM_DELETE = "CONFRIM_DELETE";
-
-export const GET_SEARCH_DATA = "GET_SEARCH_DATA";
-export const SEARCH = "SEARCH";
-
-export const GET_HOTEL_REVIEWS = "GET_HOTEL_REVIEWS";
-export const ADD_REVIEW = "ADD_REVIEW";
-export const UPDATE_REVIEW = "UPDATE_REVIEW";
-export const DELETE_REVIEW = "DELETE_REVIEW";
-export const HAS_BOOKED = "HAS_BOOKED";
-
-export const SET_SUCCESS = "SET_SUCCESS";
