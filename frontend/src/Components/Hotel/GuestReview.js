836d47c84249e5d3c2c7b571e5132d1c13ae1bd0
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-06-29 04:52:33 +0300

    update
    update

--- frontend/src/Components/Hotel/GuestReview.js
+++ frontend/src/Components/Hotel/GuestReview.js
@@ -5,7 +5,6 @@ import useSecureLs from "../Global/useSecureLs";
 function GuestReview({
     review: { user_id, rating, content, first_name, last_name, image }
 }) {
-    const [_user_id] = useSecureLs("user_id");
     let _rating = "";
     switch (rating) {
 
@@ -21,16 +20,10 @@ function GuestReview({
             <div className="flex items-center justify-between md:flex-col md:justify-start md:w-2/12 md:mr-12">
                 <img
                     src={
-                        image
-                            ? `${process.env.REACT_APP_BASE_URL}/img/users/${image}`
-                            : `${process.env.REACT_APP_BASE_URL}/img/users/avatar.png`
                     }
                     alt="avatar"
                     className="h-16 w-16 rounded-full object-cover shadow-2xl"
                 />
-                <span className="inline-block rounded-sm  text-gray-700  px-2 py-1 mt-2 text-xs text-center md:w-full">
-                    By: {first_name} {last_name}
-                </span>
                 <span className="inline-block rounded-sm  text-gray-700 bg-gray-400 px-2 py-1 mt-2 text-xs text-center md:w-full">
                     {_rating}
                 </span>
