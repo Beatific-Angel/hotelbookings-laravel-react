a027854d8ed1cf03b2216934d8ffe2ca6f7862b7
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-07-08 05:29:01 +0300

    update
    update

--- frontend/src/redux/reducers/bookings.js
+++ frontend/src/redux/reducers/bookings.js
@@ -27,7 +27,23 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
-    
+        case GET_USER_BOOKINGS: {
+            return {
+                ...state,
+                allBookings: action.payload.bookings.data,
+                pagination: {
+                    current_page: action.payload.bookings.current_page,
+                    first_page_url: action.payload.bookings.first_page_url,
+                    from: action.payload.bookings.from,
+                    last_page: action.payload.bookings.last_page,
+                    last_page_url: action.payload.bookings.last_page_url,
+                    next_page_url: action.payload.bookings.next_page_url,
+                    path: action.payload.bookings.path,
+                    per_page: action.payload.bookings.per_page,
+                    prev_page_url: action.payload.bookings.prev_page_url
+                }
+            };
+        }
         case GET_ALL_BOOKINGS: {
             return {
                 ...state,
@@ -45,7 +61,14 @@ export default function (state = initialState, action) {
                 }
             };
         }
-        
+        case ADD_BOOKING: {
+            return {
+                ...state,
+                success: action.payload.success ? true : false,
+                booking: action.payload.booking,
+                errors: action.payload.success ? null : action.payload.errors
+            };
+        }
         case SET_LOADING: {
             return {
                 ...state,
