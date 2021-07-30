2b31609310f6d14cbc0415739ac07cb7b91b866b
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-07-30 05:22:58 +0300

    update
    update

--- frontend/src/redux/actions/search.js
+++ frontend/src/redux/actions/search.js
@@ -5,6 +5,23 @@ import { setLoading } from "./global";
 const url = process.env.REACT_APP_BASE_URL;
 
 //-----------------------------------------
+export const getSearchData = (dispatch) => {
+    axios({
+        method: "get",
+        url: `${url}/api/hotels/search`,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => {
+            dispatch({
+                type: GET_SEARCH_DATA,
+                payload: response.data.data
+            });
+        })
+        .catch((error) => {});
+};
+
 //-----------------------------------------
 export const search = (dispatch, criteria, page = 1) => {
     setLoading(dispatch, true);
@@ -18,7 +35,11 @@ export const search = (dispatch, criteria, page = 1) => {
             check_in: criteria.check_in,
             check_out: criteria.check_out,
             city: criteria.city,
-            star: criteria.star
+            star: criteria.star,
+            guest: criteria.guest,
+            min_price: criteria.min_price,
+            max_price: criteria.max_price,
+            features: criteria.features
         }
     })
         .then((response) => {
